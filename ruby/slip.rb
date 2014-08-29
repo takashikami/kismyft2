00a3692cb577bf4092a45d9746d8bb0d6fa343cb
Takashi KAMI <takashi.kami@sii.co.jp>
2014-08-29 14:31:09 +0900

    enhance slip

--- ruby/slip.rb
+++ ruby/slip.rb
@@ -18,32 +18,25 @@ def bmp(filename, buf, pitch)
 end
 
 class Slip
-  attr_accessor :w,:h,:rows,:pitch,:size,:pen
+  attr_accessor :w,:h,:rows,:pitch,:size,:pen,:base
 
-  def initialize(w, h, base)
+  def initialize(w, h)
     @w = w
     @h = h
     @pitch = w/8 + (w%8 == 0 ? 0 : 1)
     @rows = ["\0"*@pitch]*h
     @size = @pitch * h
     @pen = 0
-    @base = base
-  end
-
-  def cr(delta)
-    @base+=delta
-    @pen=0
+    @base = 0
   end
 
   def draw(slot)
     x = @pen+slot.bitmap_left
     y = @base-slot.bitmap_top
     y = 0 if y < 0
-    p ["x,y =",x,y]
     xa = x/8
     shift = x%8
     b = slot.bitmap
-    p ["xa,shift =",xa,shift]
 
     rows = b.buffer.unpack("a#{b.pitch}"*b.rows)
     rows = rows.map{|r|r+"\0"}
@@ -65,18 +58,43 @@ class Slip
       ra.pack('C*')
     end
 
-    p ["advance=",slot.advance.map{|x|x>>6}]
     @pen += slot.advance[0]>>6
+    self
+  end
+
+  def face(fontfile,size)
+    @face=FT2::Face.new fontfile
+    @face.set_pixel_sizes(0,size)
+    @height=size
+    self
+  end
+
+  def type(str)
+    str.each_char do |c|
+      code = c.encode('utf-16be').unpack('n*').first
+
+      index = @face.char_index(code)
+      @face.load_glyph(index, FT2::Load::DEFAULT)
+      @face.glyph.render(FT2::RenderMode::MONO)
+
+      draw(@face.glyph)
+    end
+    self
+  end
+
+  def cr(delta=nil)
+    delta ||= @height
+    @base+=delta
+    @pen=0
+    self
   end
 end
 
-fontfile = '/usr/share/fonts/truetype/motoya-l-cedar/MTLc3m.ttf'
-#fontfile = '/usr/share/fonts/truetype/fonts-japanese-mincho.ttf'
-#fontfile = '../fonts/NotoSansJP-Thin.otf'
-#fontfile = '../fonts/NotoSansJP-Black.otf'
-#fontfile = '../fonts/NotoSansJP-Regular.otf'
-
-face=FT2::Face.new fontfile
+font_motoya = '/usr/share/fonts/truetype/motoya-l-cedar/MTLc3m.ttf'
+font_mincho = '/usr/share/fonts/truetype/fonts-japanese-mincho.ttf'
+font_thin = '../fonts/NotoSansJP-Thin.otf'
+font_black = '../fonts/NotoSansJP-Black.otf'
+font_regular = '../fonts/NotoSansJP-Regular.otf'
 
 size = ARGV[0]
 size ||= 32
@@ -84,20 +102,10 @@ size = size.to_i
 str = ARGV[1]
 str ||= 'Takashi KAMI　かみたかし'
 #str ||= (' '..'~').to_a.join
-slip = Slip.new(1024,768,size)
 
-face.set_pixel_sizes(0,size.to_i)
-asc = face.ascender*size/face.units_per_em
-des = size - asc
-p [face.ascender, face.descender, face.units_per_em, asc, des, size]
-
-str.each_char do |c|
-  code = c.encode('utf-16be').unpack('n*').first
-
-  index = face.char_index(code)
-  face.load_glyph(index, FT2::Load::DEFAULT)
-  face.glyph.render(FT2::RenderMode::MONO)
-
-  slip.draw(face.glyph)
-end
+slip = Slip.new(1024,768)
+slip.face(font_motoya,size).cr(size*4)
+slip.type(str)
+slip.face(font_thin,size*4)
+slip.type("hello")
 bmp("slip.bmp", slip.rows.join, slip.pitch)
